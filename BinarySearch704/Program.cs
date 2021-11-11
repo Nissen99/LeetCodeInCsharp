7b9a8ba0b9e06d99200351079d212e40afffb27d
Mikkel <Mikkelnj411@gmail.com>
2021-11-12 00:15:51 +0100

    Small fix

--- BinarySearch704/Program.cs
+++ BinarySearch704/Program.cs
@@ -7,9 +7,11 @@ namespace BinarySearch704
         static void Main(string[] args)
         {
             Solution solution = new Solution();
-
+            AnotherSolution anotherSolution = new AnotherSolution();
             int[] ints = new[] {-1, 0, 3, 5, 9, 12};
-            Console.WriteLine(solution.Search(ints, 2));
+            Console.WriteLine(solution.Search(ints, 3));
+            Console.WriteLine(anotherSolution.Search(ints, 3));
+
         }
     }
 }
